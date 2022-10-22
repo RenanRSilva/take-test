ee0c9e0b3d5a065ff3474fee3f83a891992361ab
RenanRSilva <renanrambuls@gmail.com>
2022-10-22 14:53:52 -0300

    http protocol  layers and tests

---
+++ src/data/use-cases/remote-load-vehicle-list.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpStatusCode } from "../protocols/http/http-client";
+import { LoadVehicleList } from "../../domain/usecases/load-vehicle-list";
+import { UnexpectedError } from "../../domain/errors/unexpected-error";
+
+export class RemoteLoadVehicleList implements LoadVehicleList {
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpClient<RemoteLoadVehicleList.Model[]>
+  ) { }
+
+  async load(): Promise<LoadVehicleList.Model[]> {
+    const httpResponse = await this.httpClient.request({
+      url: this.url,
+      method: 'get'
+    })
+    const remoteVehicles = httpResponse.body || []
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return remoteVehicles
+      case HttpStatusCode.noContent: return []
+      default: throw new UnexpectedError()
+    }
+  }
+}
+
+export namespace RemoteLoadVehicleList {
+  export type Model = {
+    evaluation_type: string,
+    version_uuid: string,
+    mileage: number,
+    model_year: number,
+    name: string,
+    transmission_type: string,
+    fuel_type: string,
+    vehicle_uuid: string,
+    model_name: string,
+    manufacturing_year: number,
+    version_name: string,
+    image: string,
+    brand_name: string,
+    ad_selling_price: number,
+    model_uuid: string,
+    brand_uuid: string
+  }
+}
---
+++ tests/data/mocks/mock-http.ts
@@ -0,0 +1,27 @@
+import { HttpRequest, HttpResponse, HttpClient, HttpStatusCode } from "../../../src/data/protocols/http/http-client"
+import { faker } from '@faker-js/faker'
+
+export const mockHttpRequest = (): HttpRequest => ({
+  url: faker.internet.url(),
+  method: faker.helpers.arrayElement(['get', 'post', 'put', 'delete']),
+  body: faker.helpers.objectKey({}),
+  headers: faker.helpers.objectKey({})
+})
+
+export class HttpClientSpy<R = any> implements HttpClient<R> {
+  url?: string
+  method?: string
+  body?: any
+  headers?: any
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok
+  }
+
+  async request(data: HttpRequest): Promise<HttpResponse<R>> {
+    this.url = data.url
+    this.method = data.method
+    this.body = data.body
+    this.headers = data.headers
+    return this.response
+  }
+}
