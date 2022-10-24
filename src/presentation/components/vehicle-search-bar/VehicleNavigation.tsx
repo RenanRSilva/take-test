cd849c0b8d15c97e3eee77a72cd8781d6ca692f6
RenanRSilva <renanrambuls@gmail.com>
2022-10-23 22:45:56 -0300

    fix: rename compoenent

--- src/presentation/components/vehicle-search-bar/VehicleNavigation.tsx
+++ src/presentation/components/vehicle-search-bar/VehicleNavigation.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 import filterIcon from '../../icons/searchbar/FilterIcon.svg'
 
-import styles from './vehicle-search-bar.module.css'
+import styles from './vehicle-navigation.module.css'
 
-export const VehicleSearchBar: React.FC = () => {
+export const VehicleNavigation: React.FC = () => {
   return (
     <nav className={styles.vehicles_navigation}>
       <div className={styles.vehicles_search_bar}>
         <h1>Veículos</h1>
-        <input placeholder="Buscar..." type="text" />
+        <input data-testid="searchbar" placeholder="Buscar..." type="text" />
       </div>
       <div className={styles.vehicles_buttons}>
         <button className={styles.vehicles_filter_button}>
